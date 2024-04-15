5aa960429139a0d6e96e02e621a2e3b145a7e73f
EduBueno <softwaredesignpaio@hotmail.com>
2024-04-15 15:32:30 -0300

    aplicativo criado pelo ionic

---
+++ myApp/config.php
@@ -0,0 +1,21 @@
+<?php
+
+$dbHost = 'serv-paio-banco-de-dados.mysql.database.azure.com';
+$dbUsername = 'PI3';
+$dbPassword = 'Paio1234.';
+$dbName = 'controle_de_clientes';
+$port = 3306;
+
+# de 'formulario-gustavo' para 'cadastro-de-clientes' #
+
+// Estabelecendo a conexão com o banco de dados no azure
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+// Verificando se houve erro na conexão
+if ($conexao->connect_errno) {
+    echo "Erro na conexão: " . $conexao->connect_error;
+    exit();
+} else {
+    echo "Conexão efetuada com sucesso";
+}
+?>
